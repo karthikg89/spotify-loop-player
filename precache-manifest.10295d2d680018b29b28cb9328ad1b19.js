e772d6b04bd6c53c43460b234ce32abbddb4e755
Karthik Gopalan <karthikgopalan@Karthiks-MacBook-Pro.local>
2025-03-08 21:22:31 -0800

    Updates

---
+++ precache-manifest.10295d2d680018b29b28cb9328ad1b19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9444550e301b5b2112ea03f8f031d5db",
+    "url": "/spotify-loop-player/index.html"
+  },
+  {
+    "revision": "8d72c2f62bee0bb9271e",
+    "url": "/spotify-loop-player/static/css/main.5ff97a2f.chunk.css"
+  },
+  {
+    "revision": "75f040930f49f86f4628",
+    "url": "/spotify-loop-player/static/js/2.216120fd.chunk.js"
+  },
+  {
+    "revision": "8d72c2f62bee0bb9271e",
+    "url": "/spotify-loop-player/static/js/main.ad4ea07e.chunk.js"
+  },
+  {
+    "revision": "074b66dac7c2d47a6cd4",
+    "url": "/spotify-loop-player/static/js/runtime~main.5326d8be.js"
+  }
+]);
